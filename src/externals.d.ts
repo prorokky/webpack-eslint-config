0003639038f6433e7687f12584bf3b607d9ed6db
pro_rokky <vlad6005@gmail.com>
2022-08-16 22:11:00 +0300

    refactor(produt): edit svg asset

--- src/externals.d.ts
+++ src/externals.d.ts
@@ -2,3 +2,13 @@ declare module '*.less' {
 	const resource: {[key: string]: string}
 	export = resource
 }
+
+declare module '*.svg' {
+	import React = require('react');
+
+	export const ReactComponent: React.SFC<React.SVGProps<SVGSVGElement>>
+
+	const url: string
+	// eslint-disable-next-line import/no-default-export
+	export default url
+}
